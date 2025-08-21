ec77145fcff273e7f43021d5c1a7991faca00fd9
Eduardo Ravi Chalita <ravichalita@gmail.com>
2025-08-21 13:14:51 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,17 @@
+
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  projectId: "caambacontrol3",
-  appId: "1:11233123437:web:edd2f4c4df82467edab364",
-  storageBucket: "caambacontrol3.firebasestorage.app",
   apiKey: "AIzaSyABRxn8A7wsLOwhl0CaZGN9ZeQiUoiLdDs",
   authDomain: "caambacontrol3.firebaseapp.com",
-  messagingSenderId: "11233123437"
+  projectId: "caambacontrol3",
+  storageBucket: "caambacontrol3.appspot.com",
+  messagingSenderId: "11233123437",
+  appId: "1:11233123437:web:edd2f4c4df82467edab364"
 };
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
