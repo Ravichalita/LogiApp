dac49b20f70f8dd897e8eb18784c24d0b35c4083
Eduardo Ravi Chalita <ravichalita@gmail.com>
2025-08-27 18:43:32 +0000

    na resolução mobile, o endereço passa a ocupar várias linhas, fazendo co

--- src/app/rentals/rental-card-actions.tsx
+++ src/app/rentals/rental-card-actions.tsx
@@ -119,24 +119,22 @@ export function RentalCardActions({ rental, status }: RentalCardActionsProps) {
   return (
     <div className="flex flex-col gap-4 h-full">
       <div className="space-y-4">
-        <div className="flex items-stretch justify-between gap-4">
-            <div className="flex items-start gap-3">
+        <div className="flex items-stretch justify-between gap-2">
+            <div className="flex items-start gap-3 flex-grow">
                 <MapPin className="h-5 w-5 text-muted-foreground mt-1 shrink-0" />
                 <div className="flex flex-col">
                     <span className="text-sm text-muted-foreground">Local de Entrega</span>
                     <span className="font-medium">{rental.deliveryAddress}</span>
                 </div>
             </div>
-             <div className="flex items-center gap-2 shrink-0">
-                {!!rental.latitude && !!rental.longitude && (
-                <Button variant="outline" size="sm" asChild className="h-auto">
-                    <Link href={`https://www.google.com/maps?q=${rental.latitude},${rental.longitude}`} target="_blank">
-                        <GoogleMapsIcon className="h-4 w-4" />
-                        <span>Abrir no Mapa</span>
-                    </Link>
-                </Button>
-                )}
-            </div>
+            {!!rental.latitude && !!rental.longitude && (
+            <Button variant="outline" size="sm" asChild className="h-auto">
+                <Link href={`https://www.google.com/maps?q=${rental.latitude},${rental.longitude}`} target="_blank">
+                    <GoogleMapsIcon className="h-4 w-4" />
+                    <span>Abrir no Mapa</span>
+                </Link>
+            </Button>
+            )}
         </div>
         <div className="flex items-center justify-between">
             <div className="flex items-start gap-3">
